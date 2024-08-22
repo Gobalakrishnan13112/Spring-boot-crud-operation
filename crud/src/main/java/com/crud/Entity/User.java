00353d8c0dd14304cf4e6d22308a50faf96f26a0
Gobalakrishnan13112 <gobalakrishnan13112@gmail.com>
2024-08-22 21:09:54 +0530

    Add files via upload

---
+++ crud/src/main/java/com/crud/Entity/User.java
@@ -0,0 +1,39 @@
+package com.crud.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	@Column(name = "username")
+	String name;
+	
+	@Column(name="email")
+	String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
