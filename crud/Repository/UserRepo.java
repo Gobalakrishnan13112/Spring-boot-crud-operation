6852ed0f7dfb7eeed9f3d9fcefbb2e40ec0a9a3e
Gobalakrishnan13112 <gobalakrishnan13112@gmail.com>
2024-08-22 07:50:48 +0530

    Add files via upload

---
+++ crud/Repository/UserRepo.java
@@ -0,0 +1,13 @@
+package com.crud.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crud.Entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+	User findByEmail(String email);
+
+}
