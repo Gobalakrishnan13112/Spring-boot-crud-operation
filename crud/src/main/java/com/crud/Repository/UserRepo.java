b73f586ac8a05fb309f08ec38b53f1640f97d314
Gobalakrishnan13112 <gobalakrishnan13112@gmail.com>
2024-08-22 21:11:48 +0530

    Update UserRepo.java

--- crud/src/main/java/com/crud/Repository/UserRepo.java
+++ crud/src/main/java/com/crud/Repository/UserRepo.java
@@ -2,7 +2,6 @@ package com.crud.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.crud.Entity.User;
 
 @Repository
